6d8a4965e981d7ddd74275733570ba9796053fcd
anto991nio <antonio.91.cardone@gmail.com>
2021-06-23 15:34:27 +0200

    Termina esercizio

---
+++ database/migrations/2021_06_23_130443_create_travels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string("nome pacchetto", 100);
+            $table->string("luogo", 100);
+            $table->longText("descrizione");
+            $table->tinyInteger("giorni inclusi");
+            $table->integer("costo");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
